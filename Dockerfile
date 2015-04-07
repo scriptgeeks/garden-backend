e2f23d1394163afb6a1327900a36bc3117ef613d
chenzhan <chenzhan@taobao.com>
2015-04-07 14:32:30 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM java:8
+ADD build/libs/gs-spring-boot-0.1.0.jar /opt/local/
---
+++ src/main/java/co/scriptgeek/garden/HelloController.java
@@ -0,0 +1,14 @@
+package co.scriptgeek.garden;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot! v2";
+    }
+
+}
